02a248a48e3b667d837021e881292581f62d5b46
Michael Iline <michael.iline@ironsrc.com>
2020-04-17 00:42:56 +0300

    test(jest): add jest testing framework

--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -1,34 +1,54 @@
-const define = require('requirejs')
+const utils = require('../../lib/utils')
+const fs = require('fs')
+jest.mock('fs')
 
-define.config({
-  baseUrl: __dirname
-});
+describe('utils module', function () {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  describe('createDirectory', function () {
+    it('should create directory, when directory not exist', () => {
+      //Arrange
+      fs.existsSync.mockReturnValue(false)
+      //Action
+      utils.createDirectory('directoryToCreate')
+      //Assert
+      expect(fs.mkdir).toHaveBeenCalled()
+    })
 
-const testr = require('../../unit/lib/testr');
-
-testr.config({
-  root: '../',
-  baseUrl: 'lib',
-  specUrl: 'test/unit',
-  stubUrl: 'stub',
-  ignore: ['jquery', 'underscore'],
-  whitelist: ['path/to/allowed/actual', 'underscore', 'backbone']
-})
-
-const test = require('ava')
-
-define([], (testr) => {
-
-  // module instancing
-  const utils = testr('utils', { 'fs': {} })
-
-  test('foo', t => {
-    utils.isFileExists('path')
-    t.pass()
+    it('should not create directory, when directory exist', () => {
+      //Arrange
+      fs.existsSync.mockReturnValue(true)
+      //Action
+      utils.createDirectory('directoryToCreate')
+      //Assert
+      expect(fs.mkdir).not.toHaveBeenCalled()
+    })
   })
 
-  test('bar', async t => {
-    const bar = Promise.resolve('bar')
-    t.is(await bar, 'bar')
+  describe('deleteFile', function () {
+    it('should delete file, when file exist', () => {
+      //Arrange
+      fs.existsSync.mockReturnValue(true)
+
+      //Action
+      utils.deleteFile('aaa/filetodelete')
+
+      //Assert
+      expect(fs.unlinkSync).toHaveBeenCalled()
+      expect(fs.unlinkSync).toHaveBeenCalledWith('aaa/filetodelete')
+    })
+
+    it('should not try to delete file, when file not exist', () => {
+      //Arrange
+      fs.existsSync.mockReturnValue(false)
+
+      //Action
+      utils.deleteFile('aaa/filetodelete')
+
+      //Assert
+      expect(fs.unlinkSync).not.toHaveBeenCalled()
+    })
   })
 })
+
