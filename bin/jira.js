fd5df9ca65937401b34dd8baaa1c01bfac71908a
Michael Iline <michael.iline@ironsrc.com>
2020-04-17 11:34:26 +0300

    wip(bin/jira): add nodescript description

--- bin/jira.js
+++ bin/jira.js
@@ -1,4 +1,5 @@
-/*global requirejs,define, fs*/
+#!/usr/bin/env node
+
 //
 // Main API       : https://docs.atlassian.com/software/jira/docs/api/REST/8.1.0/
 // Creating Issues: https://developer.atlassian.com/jiradev/jira-apis/about-the-jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-examples#JIRARESTAPIexamples-Creatinganissueusingcustomfields
@@ -6,6 +7,8 @@
 // Required Fields: https://jira.mypaytm.com/rest/api/2/issue/createmeta?projectKeys=MDO&expand=projects.issuetypes.fields&
 // Meta-data      : http://localhost:8080/rest/api/2/issue/JRA-13/editmeta
 //
+
+
 module.exports = function () {
   var program = require('commander');
 
